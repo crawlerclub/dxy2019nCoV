83c0d7fac187662ef62152170715d2386205c6dc
Zhanliang Liu <zliu@ruc.edu.cn>
2020-02-07 11:45:20 +0800

    update get.sh

--- bin/get.sh
+++ bin/get.sh
@@ -10,8 +10,5 @@ ts=`date +%Y%m%d%H%M`
 echo $day >> git.log
 wget ${url}  -O ../data/${ts}.html
 git add ../data/${ts}.html
-python citycnt.py
-echo "python citycnt.py" >> py.log
-git add ../cities/${ts}.json
 git commit -m "get data for ${ts}" >> git.log
 git push >> git.log
